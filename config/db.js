81b266cd99370384b446d289db40ca908e139e13
Shahbaj Singh <ssingh@comqi.com>
2024-02-29 21:30:45 -0500

    Initial push

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql=require("mysql");
+require('dotenv').config();
+
+const con = mysql.createConnection({
+    host:process.env.DB_HOST,
+        port:process.env.DB_PORT,
+        user:process.env.DB_USERNAME,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_DATABASE,
+});
+
+
+module.exports = con;
---
+++ index.js
@@ -0,0 +1,42 @@
+const uidlib =require('uid');
+const express = require("express");
+const data= require("./data");
+const con = require("./config/db");
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+'\\static\\main.htm');
+});
+
+app.post("/api/linkgenerator",(req,res)=>{
+    shortUrlID = uidlib.uid(8);
+    longUrl = req.body.longURL;
+    console.log(shortUrlID+"     "+longUrl);
+    con.query(`insert into urlmap values("${shortUrlID}","${longUrl}")`,(err,result)=>{
+        if(err) console.log(err);
+    })
+
+    res.json({short:shortUrlID,long:longUrl});
+})
+
+app.get("/:uid",(req,res)=>{
+    uid = req.params.uid;
+    con.query(`select * from urlmap where urlID = "${uid}"`,(error, result)=>{
+        if(result.length!=0){
+            res.redirect(result[0].longurl);
+        }else{
+            res.status(404);
+            res.send("Not worked!");
+            console.error("Not Found")
+        }
+    })
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log("Our website is hosting at http://localhost:"+process.env.PORT);
+});
